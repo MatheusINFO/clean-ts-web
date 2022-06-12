b0a9931058ac7112800b85a6b60654a265fbfd8f
Matheus Andrade <matheus.add12@gmail.com>
2022-06-12 16:48:10 -0300

    feat: prevent Login to submit form if its invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -149,4 +149,15 @@ describe('LoginComponent', () => {
 
     expect(authenticationSpy.callsCount).toBe(1)
   })
+
+  it('Should not call Authentication if form is invalid', () => {
+    const validationError = faker.random.words()
+    const { sut, authenticationSpy } = makeSut({
+      validationError,
+    })
+    populateInputField(sut, 'email', faker.internet.email())
+    fireEvent.submit(sut.getByTestId('form'))
+
+    expect(authenticationSpy.callsCount).toBe(0)
+  })
 })
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -38,7 +38,7 @@ const Login: React.FC<Props> = ({ validation, authentication }) => {
   ): Promise<void> => {
     event.preventDefault()
 
-    if (state.isLoading) {
+    if (state.isLoading || state.emailError || state.passwordError) {
       return
     }
 
@@ -58,7 +58,10 @@ const Login: React.FC<Props> = ({ validation, authentication }) => {
       <UnsignedHeader />
 
       <Context.Provider value={{ state, setState }}>
-        <form className={Styles.form} onSubmit={handleSubmit}>
+        <form
+          data-testid="form"
+          className={Styles.form}
+          onSubmit={handleSubmit}>
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
           <Input
