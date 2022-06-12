1b7931c79a5bb5c2065b71cae896850f5a6cd67e
Matheus Andrade <matheus.add12@gmail.com>
2022-06-12 15:44:56 -0300

    feat: ensure Login present valid state if email is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -65,6 +65,18 @@ describe('LoginComponent', () => {
     expect(passwordStatus.textContent).toBe('🙁')
   })
 
+  it('Should show valid email state if Validation succeds', () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, {
+      target: { value: faker.internet.email() },
+    })
+    const emailStatus = sut.getByTestId('email-status')
+    expect(emailStatus.title).toBe('Tudo certo!')
+    expect(emailStatus.textContent).toBe('🙂')
+  })
+
   it('Should show valid password state if Validation succeds', () => {
     const { sut, validationStub } = makeSut()
     validationStub.errorMessage = null
