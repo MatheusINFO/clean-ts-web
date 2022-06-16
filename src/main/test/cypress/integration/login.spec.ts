c6f5a5eeacb6176572279f8ece287a6b00d0bf80
Matheus Andrade <matheus.add12@gmail.com>
2022-06-15 22:18:19 -0300

    test: add login e2e

---
+++ src/main/test/cypress/integration/login.spec.ts
@@ -0,0 +1,98 @@
+import faker from 'faker'
+
+const baseUrl: string = Cypress.config().baseUrl
+
+describe('Login', () => {
+  beforeEach(() => {
+    cy.visit('login')
+  })
+
+  it('Should load with correct initial state', () => {
+    cy.getByTestId('email').should('have.attr', 'readonly')
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🙁')
+
+    cy.getByTestId('password').should('have.attr', 'readonly')
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🙁')
+
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('email').focus().type(faker.random.word())
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'O campo email é inválido')
+      .should('contain.text', '🙁')
+
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3))
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'O campo password é inválido')
+      .should('contain.text', '🙁')
+
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present valid state if form is valid', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Tudo certo!')
+      .should('contain.text', '🙂')
+
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Tudo certo!')
+      .should('contain.text', '🙂')
+
+    cy.getByTestId('submit').should('not.have.attr', 'disabled')
+
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error if invalid credentials are provided', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').click()
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner')
+      .should('exist')
+      .getByTestId('main-error')
+      .should('not.exist')
+
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner')
+      .should('not.exist')
+      .getByTestId('main-error')
+      .should('contain.text', 'Credenciais inválidas')
+
+    cy.url().should('eq', `${baseUrl}/login`)
+  })
+
+  it('Should present save accesstoken if valid credentiasl are provided', () => {
+    cy.getByTestId('email').focus().type('matheusinfo@github.com')
+    cy.getByTestId('password').focus().type('12345')
+    cy.getByTestId('submit').click()
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner')
+      .should('exist')
+      .getByTestId('main-error')
+      .should('not.exist')
+
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner')
+      .should('not.exist')
+      .getByTestId('main-error')
+      .should('contain.text', 'not.exists')
+
+    cy.url().should('eq', `${baseUrl}/`)
+    cy.window().then((window) =>
+      assert.isOk(window.localStorage.getItem('accessToken'))
+    )
+  })
+})
