a4cf77899d3803192d5c79b76466a533be90fdfd
Matheus Andrade <matheus.add12@gmail.com>
2022-06-16 16:50:36 -0300

    test: add e2e signup

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -1,20 +1,24 @@
 import faker from 'faker'
 import {
-  mockInvalidCredentialsError,
-  mockInvalidData,
-  mockSuccess,
-  mockUnexpectedError,
-} from '../support/login-mocks'
-import {
   testHttpCallsCount,
   testInputStatus,
   testMainError,
 } from '../support/form-helper'
+import {
+  mockEmailInUseError,
+  mockInvalidData,
+  mockUnexpectedError,
+  mockSuccess,
+} from '../support/signup-mocks'
 import { testLocalStorageItem, testUrl } from '../support/http-mocks'
 
 const populateFields = (): void => {
+  const password = faker.internet.password()
+
+  cy.getByTestId('name').focus().type(faker.name.findName())
   cy.getByTestId('email').focus().type(faker.internet.email())
-  cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+  cy.getByTestId('password').focus().type(password)
+  cy.getByTestId('passwordConfirmation').focus().type(password)
 }
 
 const simulateValidSubmit = (): void => {
@@ -22,56 +26,81 @@ const simulateValidSubmit = (): void => {
   cy.getByTestId('submit').click()
 }
 
-describe('Login', () => {
+describe('SignUp', () => {
   beforeEach(() => {
-    cy.visit('login')
+    cy.visit('signup')
   })
 
   it('Should load with correct initial state', () => {
+    testInputStatus('name', 'Campo obrigatório')
+    cy.getByTestId('name').should('have.attr', 'readOnly')
+
     testInputStatus('email', 'Campo obrigatório')
     cy.getByTestId('email').should('have.attr', 'readOnly')
 
     testInputStatus('password', 'Campo obrigatório')
     cy.getByTestId('password').should('have.attr', 'readOnly')
 
+    testInputStatus('passwordConfirmation', 'Campo obrigatório')
+    cy.getByTestId('passwordConfirmation').should('have.attr', 'readOnly')
+
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 
   it('Should present error state if form is invalid', () => {
+    cy.getByTestId('name').focus().type(faker.random.alphaNumeric(2))
+    testInputStatus('name', 'O campo name é inválido')
+
     cy.getByTestId('email').focus().type(faker.random.word())
     testInputStatus('email', 'O campo email é inválido')
 
     cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3))
     testInputStatus('password', 'O campo password é inválido')
 
+    cy.getByTestId('passwordConfirmation')
+      .focus()
+      .type(faker.random.alphaNumeric(2))
+    testInputStatus(
+      'passwordConfirmation',
+      'O campo passwordConfirmation é inválido'
+    )
+
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 
   it('Should present valid state if form is valid', () => {
+    const password = faker.internet.password()
+
+    cy.getByTestId('name').focus().type(faker.name.findName())
+    testInputStatus('name')
+
     cy.getByTestId('email').focus().type(faker.internet.email())
     testInputStatus('email')
 
-    cy.getByTestId('password').focus().type(faker.internet.password())
+    cy.getByTestId('password').focus().type(password)
     testInputStatus('password')
+
+    cy.getByTestId('passwordConfirmation').focus().type(password)
+    testInputStatus('passwordConfirmation')
 
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 
-  it('Should present error if invalid credentials are provided', () => {
-    mockInvalidCredentialsError()
+  it('Should present error if a email that are already in use are provided', () => {
+    mockEmailInUseError()
     simulateValidSubmit()
-    testMainError('Credenciais inválidas')
-    testUrl('/login')
+    testMainError('Esse e-mail já está em uso!')
+    testUrl('/signup')
   })
 
   it('Should present error on UnexpectedError', () => {
     mockUnexpectedError()
     simulateValidSubmit()
     testMainError('Algo de errado aconteceu. Tente novamente!')
-    testUrl('/login')
+    testUrl('/signup')
   })
 
   it('Should present UnexpectedError if invalid date is returned', () => {
@@ -91,6 +120,7 @@ describe('Login', () => {
     mockSuccess()
     populateFields()
     cy.getByTestId('submit').dblclick()
+
     testHttpCallsCount(1)
   })
 
---
+++ src/main/test/cypress/support/signup-mocks.ts
@@ -0,0 +1,20 @@
+import faker from 'faker'
+import * as Helper from './http-mocks'
+
+export const mockEmailInUseError = (): void =>
+  Helper.mockEmailInUseError(/signup/)
+
+export const mockUnexpectedError = (): void =>
+  Helper.mockUnexpectedError(/signup/, 'POST')
+
+export const mockInvalidData = (): void => {
+  Helper.mockSuccess(/signup/, 'POST', {
+    [faker.random.word()]: faker.random.uuid(),
+  })
+}
+
+export const mockSuccess = (): void => {
+  Helper.mockSuccess(/signup/, 'POST', {
+    accessToken: faker.random.uuid(),
+  })
+}
