4c02dfe0f979b3447e654d04f0a5ad67515b2a5e
Matheus Andrade <matheus.add12@gmail.com>
2022-06-17 16:43:01 -0300

    feat: add skeleton shimmer effect

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -21,36 +21,7 @@ const SurveyList: React.FC = () => (
 
             <p>Qual é seu framework web favorito?</p>
           </div>
-          <footer>Ver Resultado</footer>
-        </li>
 
-        <li>
-          <div className={Styles.surveyContent}>
-            <Icon className={Styles.iconWrap} iconName={IconName.thumbUp} />
-
-            <time>
-              <span className={Styles.day}>16</span>
-              <span className={Styles.month}>06</span>
-              <span className={Styles.year}>2022</span>
-            </time>
-
-            <p>Qual é seu framework web favorito?</p>
-          </div>
-          <footer>Ver Resultado</footer>
-        </li>
-
-        <li>
-          <div className={Styles.surveyContent}>
-            <Icon className={Styles.iconWrap} iconName={IconName.thumbUp} />
-
-            <time>
-              <span className={Styles.day}>16</span>
-              <span className={Styles.month}>06</span>
-              <span className={Styles.year}>2022</span>
-            </time>
-
-            <p>Qual é seu framework web favorito?</p>
-          </div>
           <footer>Ver Resultado</footer>
         </li>
       </ul>
