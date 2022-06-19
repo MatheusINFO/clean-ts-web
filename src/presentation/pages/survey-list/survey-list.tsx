dbc16964f66d67e53ee36f6ed2f1578a54a13acb
Matheus Andrade <matheus.add12@gmail.com>
2022-06-18 22:30:36 -0300

    feat: ensure SurveyListComponent calls LoadSurveyList

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Footer, SignedHeader } from '@/presentation/components'
 import Styles from './survey-list-styles.scss'
 import { SurveyItemEmpty } from './components'
+import { LoadSurveyList } from '@/domain/usecases'
 
-const SurveyList: React.FC = () => (
-  <div className={Styles.surveyListWrap}>
-    <SignedHeader />
+type Props = {
+  loadSurveyList: LoadSurveyList
+}
 
-    <div className={Styles.contentWrap}>
-      <h2>Enquetes</h2>
-      <ul data-testid="survey-list">
-        <SurveyItemEmpty />
-      </ul>
+const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
+  useEffect(() => {
+    ;(async function () {
+      await loadSurveyList.loadAll()
+    })()
+  }, [])
+
+  return (
+    <div className={Styles.surveyListWrap}>
+      <SignedHeader />
+
+      <div className={Styles.contentWrap}>
+        <h2>Enquetes</h2>
+        <ul data-testid="survey-list">
+          <SurveyItemEmpty />
+        </ul>
+      </div>
+
+      <Footer />
     </div>
-
-    <Footer />
-  </div>
-)
+  )
+}
 
 export default SurveyList
