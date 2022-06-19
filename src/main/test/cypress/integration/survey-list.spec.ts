9922ed7893ee1569e2c42dfeae84ba739e772cbd
Matheus Andrade <matheus.add12@gmail.com>
2022-06-19 19:22:13 -0300

    test: ensure SurveyList on logout link click

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -1,5 +1,9 @@
 import faker from 'faker'
-import { getLocalStorageItem, setLocalStorageItem } from '../support/helpers'
+import {
+  getLocalStorageItem,
+  setLocalStorageItem,
+  testUrl,
+} from '../support/helpers'
 import {
   mockUnexpectedError,
   mockAccessDeniedError,
@@ -24,7 +28,8 @@ describe('SurveyList', () => {
 
   it('Should logout on AccessDeniedError', () => {
     mockAccessDeniedError()
-    cy.visit('/login')
+    cy.visit('/')
+    testUrl('/login')
   })
 
   it('Should present correct username', () => {
@@ -32,5 +37,12 @@ describe('SurveyList', () => {
     cy.visit('/')
     const { name } = getLocalStorageItem('account')
     cy.getByTestId('username').should('contain.text', name)
+  })
+
+  it('Should logout on logout link click', () => {
+    mockUnexpectedError()
+    cy.visit('/')
+    cy.getByTestId('logout').click()
+    testUrl('/login')
   })
 })
