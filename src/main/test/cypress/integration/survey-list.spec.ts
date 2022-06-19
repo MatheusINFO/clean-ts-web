0536cbced4a4d3168300083939101dc0fb6055e9
Matheus Andrade <matheus.add12@gmail.com>
2022-06-19 20:05:44 -0300

    test: ensure SurveyList reload on button click

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -7,10 +7,11 @@ import {
 
 const path = /surveys/
 
-export const mockUnexpectedError = (): void => Http.mockServerError(path, 'GET')
+const mockUnexpectedError = (): void => Http.mockServerError(path, 'GET')
 
-export const mockAccessDeniedError = (): void =>
-  Http.mockForbiddenError(path, 'GET')
+const mockAccessDeniedError = (): void => Http.mockForbiddenError(path, 'GET')
+
+const mockSuccess = (): void => Http.mockSuccess(path, 'GET', 'fx:survey-list')
 
 describe('SurveyList', () => {
   beforeEach(() => {
@@ -26,6 +27,18 @@ describe('SurveyList', () => {
       'contain.text',
       'Algo de errado aconteceu. Tente novamente!'
     )
+  })
+
+  it('Should reload on button click', () => {
+    mockUnexpectedError()
+    cy.visit('/')
+    cy.getByTestId('error').should(
+      'contain.text',
+      'Algo de errado aconteceu. Tente novamente!'
+    )
+    mockSuccess()
+    cy.getByTestId('reload').click()
+    cy.get('li:not(:empty)').should('have.length', 2)
   })
 
   it('Should logout on AccessDeniedError', () => {
@@ -46,5 +59,21 @@ describe('SurveyList', () => {
     cy.visit('/')
     cy.getByTestId('logout').click()
     testUrl('/login')
+  })
+
+  it('Should present survey items', () => {
+    mockSuccess()
+    cy.visit('/')
+    cy.get('li:empty').should('have.length', 4)
+    cy.get('li:not(:empty)').should('have.length', 2)
+    cy.get('li:nth-child(1)').then((li) => {
+      assert.equal(li.find('[data-testid="day"]').text(), '03')
+      assert.equal(li.find('[data-testid="month"]').text(), 'fev')
+      assert.equal(li.find('[data-testid="year"]').text(), '2018')
+      assert.equal(li.find('[data-testid="question"]').text(), 'Question 1')
+      cy.fixture('icons').then((icon) => {
+        assert.equal(li.find('[data-testid="icon"]').attr('src'), icon.thumbUp)
+      })
+    })
   })
 })
