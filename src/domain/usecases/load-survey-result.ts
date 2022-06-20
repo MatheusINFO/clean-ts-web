74319e639deac5d158f8cece4ea0c0d941f885f2
Matheus Andrade <matheus.add12@gmail.com>
2022-06-20 20:55:25 -0300

    feat: add LoadSurveyResult usecase

---
+++ src/domain/usecases/load-survey-result.ts
@@ -0,0 +1,16 @@
+export interface LoadSurveyResult {
+  load: () => Promise<LoadSurveyResult.Result>
+}
+
+export namespace LoadSurveyResult {
+  export type Result = {
+    question: string
+    date: Date
+    answers: Array<{
+      answer: string
+      count: number
+      percent: number
+      image?: string
+    }>
+  }
+}
