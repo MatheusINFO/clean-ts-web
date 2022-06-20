47f113b1cd61ea70bbc637847642ebfe2af3bcba
Matheus Andrade <matheus.add12@gmail.com>
2022-06-19 22:33:02 -0300

    chore: add webpack prod and dev configs

---
+++ webpack.dev.js
@@ -0,0 +1,47 @@
+const { DefinePlugin } = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const merge = require('webpack-merge')
+const common = require('./webpack.common')
+
+module.exports = merge(common, {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: './public',
+    writeToDisk: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new DefinePlugin({
+      API_URL: JSON.stringify('http://fordevs.herokuapp.com/api'),
+    }),
+    new HtmlWebPackPlugin({
+      template: './template.dev.html',
+    }),
+  ],
+})
