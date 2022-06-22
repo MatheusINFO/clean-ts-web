c7856e9973d7ba1cf795cd99343444723b9bb949
Matheus Andrade <matheus.add12@gmail.com>
2022-06-21 21:13:09 -0300

    test: ensure SurveyResult redirects to login if no token is found

--- src/main/test/cypress/integration/private-routes.spec.ts
+++ src/main/test/cypress/integration/private-routes.spec.ts
@@ -5,4 +5,9 @@ describe('PrivateRoutes', () => {
     cy.visit('')
     testUrl('/login')
   })
+
+  it('Should logout if survey-result has no token', () => {
+    cy.visit('/surveys/any_id')
+    testUrl('/login')
+  })
 })
