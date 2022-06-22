34a8601abd37ff3908beaa662993b086ccbaeefd
Matheus Andrade <matheus.add12@gmail.com>
2022-06-22 12:47:59 -0300

    test: ensure SurveyResult logout on AccessDeniedError

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -93,6 +93,8 @@ describe('SurveyResult', () => {
 
   describe('save', () => {
     const mockUnexpectedError = (): void => Http.mockServerError(path, 'PUT')
+    const mockAccessDeniedError = (): void =>
+      Http.mockForbiddenError(path, 'PUT')
 
     beforeEach(() => {
       cy.fixture('account').then((account) =>
@@ -109,6 +111,12 @@ describe('SurveyResult', () => {
         'contain.text',
         'Algo de errado aconteceu. Tente novamente!'
       )
+    })
+
+    it('Should logout on AccessDeniedError', () => {
+      mockAccessDeniedError()
+      cy.get('li:nth-child(2)').click()
+      testUrl('/login')
     })
   })
 })
