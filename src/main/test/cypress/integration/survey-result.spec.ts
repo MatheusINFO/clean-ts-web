a9cf2a606914240f61e7271738a7ab4fcd24e579
Matheus Andrade <matheus.add12@gmail.com>
2022-06-22 12:52:03 -0300

    test: ensure SurveyResult present new result on answer click

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -8,7 +8,7 @@ import {
 const path = /surveys/
 
 const mockLoadSuccess = (): void =>
-  Http.mockSuccess(path, 'GET', 'fx:survey-result')
+  Http.mockSuccess(path, 'GET', 'fx:load-survey-result')
 
 describe('SurveyResult', () => {
   describe('load', () => {
@@ -95,6 +95,8 @@ describe('SurveyResult', () => {
     const mockUnexpectedError = (): void => Http.mockServerError(path, 'PUT')
     const mockAccessDeniedError = (): void =>
       Http.mockForbiddenError(path, 'PUT')
+    const mockSaveSuccess = (): void =>
+      Http.mockSuccess(path, 'PUT', 'fx:save-survey-result')
 
     beforeEach(() => {
       cy.fixture('account').then((account) =>
@@ -117,6 +119,29 @@ describe('SurveyResult', () => {
       mockAccessDeniedError()
       cy.get('li:nth-child(2)').click()
       testUrl('/login')
+    })
+
+    it('Should present survey result', () => {
+      mockSaveSuccess()
+      cy.get('li:nth-child(2)').click()
+
+      cy.getByTestId('question').should('have.text', 'Other Question')
+      cy.getByTestId('day').should('have.text', '23')
+      cy.getByTestId('month').should('have.text', 'mar')
+      cy.getByTestId('year').should('have.text', '2020')
+      cy.get('li:nth-child(1)').then((li) => {
+        assert.equal(li.find('[data-testid="answer"]').text(), 'other_answer')
+        assert.equal(
+          li.find('[data-testid="image"]').attr('src'),
+          'other_image'
+        )
+        assert.equal(li.find('[data-testid="percent"]').text(), '50%')
+      })
+      cy.get('li:nth-child(2)').then((li) => {
+        assert.equal(li.find('[data-testid="answer"]').text(), 'other_answer2')
+        assert.notExists(li.find('[data-testid="image"]'))
+        assert.equal(li.find('[data-testid="percent"]').text(), '50%')
+      })
     })
   })
 })
