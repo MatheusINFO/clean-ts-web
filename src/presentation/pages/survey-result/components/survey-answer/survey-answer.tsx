96901702ad479fc0ff16fc7bd2b4c46e4c57ba07
Matheus Andrade <matheus.add12@gmail.com>
2022-06-21 21:09:01 -0300

    refactor: separate folder in components

---
+++ src/presentation/pages/survey-result/components/survey-answer/survey-answer.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Styles from './survey-answer-styles.scss'
+
+type Props = {
+  answer: {
+    answer: string
+    count: number
+    percent: number
+    image?: string
+    isCurrentAccountAnswer: boolean
+  }
+}
+
+const SurveyAnswer: React.FC<Props> = ({ answer }: Props) => {
+  const activeClassName = answer.isCurrentAccountAnswer ? Styles.active : ''
+
+  return (
+    <li
+      data-testid="answer-wrap"
+      className={[Styles.surveyAnswerWrap, activeClassName].join(' ')}
+      key={answer.answer}>
+      {answer.image && (
+        <img data-testid="image" src={answer.image} alt={answer.answer} />
+      )}
+      <span data-testid="answer" className={Styles.answer}>
+        {answer.answer}
+      </span>
+      <span data-testid="percent" className={Styles.percent}>
+        {answer.percent}%
+      </span>
+    </li>
+  )
+}
+
+export default SurveyAnswer
--- src/presentation/pages/survey-result/components/survey-result-data/survey-result-data.tsx
+++ src/presentation/pages/survey-result/components/survey-result-data/survey-result-data.tsx
@@ -4,6 +4,7 @@ import { useHistory } from 'react-router-dom'
 import { Calendar } from '@/presentation/components'
 import { LoadSurveyResult } from '@/domain/usecases'
 import Styles from './survey-result-data-styles.scss'
+import { SurveyResultAnswer } from '@/presentation/pages/survey-result/components'
 
 type Props = {
   surveyResult: LoadSurveyResult.Result
@@ -20,22 +21,11 @@ const SurveyResultData: React.FC<Props> = ({ surveyResult }: Props) => {
       </hgroup>
 
       <FlipMove data-testid="answers" className={Styles.answerList}>
-        {surveyResult.answers.map((answer) => (
-          <li
-            data-testid="answer-wrap"
-            className={answer.isCurrentAccountAnswer ? Styles.active : ''}
-            key={answer.answer}>
-            {answer.image && (
-              <img data-testid="image" src={answer.image} alt={answer.answer} />
-            )}
-            <span data-testid="answer" className={Styles.answer}>
-              {answer.answer}
-            </span>
-            <span data-testid="percent" className={Styles.percent}>
-              {answer.percent}%
-            </span>
-          </li>
-        ))}
+        <>
+          {surveyResult.answers.map((answer) => (
+            <SurveyResultAnswer key={answer.answer} answer={answer} />
+          ))}
+        </>
       </FlipMove>
 
       <button
